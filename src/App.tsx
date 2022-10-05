47513a67030e1a31ebe6d8262d2ee8610ed1041b
hisham <hishammpsn@gmail.com>
2022-10-05 14:12:56 +0530

    add todo created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Input from './components/Input';
+import { Todo } from './modal';
 
 const App:React.FC = () => {
+  const [todo,setTodo] = useState<string>("")
+  const [todos,setTodos] = useState<Todo[]>([])
+
+  const handleSubmit = (e:React.FormEvent) =>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+      setTodo("")
+    }
+  }
+  console.log(todos)
   return (
     <div className='App'>
       <span className="heading" >Taskify</span>
-      <Input/>
+      <Input todo={todo} setTodo={setTodo} handleSumbit={handleSubmit} />
     </div>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
-const Input = () => {
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleSumbit: (e: React.FormEvent) => void;
+}
+
+const Input = ({ todo, setTodo, handleSumbit }: Props) => {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
-   <form className='input' >
-    <input type='input' placeholder='Enter a task' 
-    className="input__box"/>
-    <button type='submit' className='input_submit' >Go</button>
-   </form>
+    <form className='input' onSubmit={(e) => {
+      handleSumbit(e);
+      inputRef.current?.blur();
+    }}>
+      <input type='input'
+        ref={inputRef}
+        value={todo}
+        placeholder='Enter a task'
+        className="input__box"
+        onChange={(e) => setTodo(e.target.value)} />
+      <button type='submit' className='input_submit' >Go</button>
+    </form>
   )
 }
 
